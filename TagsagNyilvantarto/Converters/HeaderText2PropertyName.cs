8ce02475b26cbbc0be101620ecc93e8354689f1b
m1cr0waveoven <51439076+m1cr0waveoven@users.noreply.github.com>
2024-07-28 16:09:30 +0200

    Refactored HeaderText2PropertyName.cs

--- TagsagNyilvantarto/Converters/HeaderText2PropertyName.cs
+++ TagsagNyilvantarto/Converters/HeaderText2PropertyName.cs
@@ -6,36 +6,36 @@ using System.Windows.Data;
 namespace TagsagNyilvantarto.Converters
 {
     [ValueConversion(typeof(string), typeof(string))]
-    internal class HeaderText2PropertyName : IValueConverter
+    internal sealed class HeaderText2PropertyName : IValueConverter
     {
-        private Dictionary<string, string> keyValuePairs = new Dictionary<string, string>(capacity: 9);
+        private readonly Dictionary<string, string> _headerNameToPropertyName = new Dictionary<string, string>(capacity: 9);
         public HeaderText2PropertyName()
         {
-            keyValuePairs.Add("Id", "DataAccess.Idk");
-            keyValuePairs.Add("Név", "DataAccess.Nevek");
-            keyValuePairs.Add("Születés", "DataAccess.Szuletesek");
-            keyValuePairs.Add("Email", "DataAccess.Emailek");
-            keyValuePairs.Add("Telefon", "DataAccess.Telefonok");
-            keyValuePairs.Add("Tisztség", "DataAccess.Tisztsegek");
-            keyValuePairs.Add("Tagság kezdete", "DataAccess.Tagsagkezdetek");
-            keyValuePairs.Add("Tagság jogállása", "DataAccess.Tagsagjogallasok");
-            keyValuePairs.Add("AdatokTípusa", "DataAccess.Adattipusok");
-            keyValuePairs.Add("Képviselő", "DataAccess.Kepviselo");
-            keyValuePairs.Add("Admin", "DataAccess.Admin");
+            _headerNameToPropertyName.Add("Id", "DataAccess.Idk");
+            _headerNameToPropertyName.Add("Név", "DataAccess.Nevek");
+            _headerNameToPropertyName.Add("Születés", "DataAccess.Szuletesek");
+            _headerNameToPropertyName.Add("Email", "DataAccess.Emailek");
+            _headerNameToPropertyName.Add("Telefon", "DataAccess.Telefonok");
+            _headerNameToPropertyName.Add("Tisztség", "DataAccess.Tisztsegek");
+            _headerNameToPropertyName.Add("Tagság kezdete", "DataAccess.Tagsagkezdetek");
+            _headerNameToPropertyName.Add("Tagság jogállása", "DataAccess.Tagsagjogallasok");
+            _headerNameToPropertyName.Add("AdatokTípusa", "DataAccess.Adattipusok");
+            _headerNameToPropertyName.Add("Képviselő", "DataAccess.Kepviselo");
+            _headerNameToPropertyName.Add("Admin", "DataAccess.Admin");
         }
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (!(value is String))
                 return value;
 
-            keyValuePairs.TryGetValue((string)value, out string propname);
+            _headerNameToPropertyName.TryGetValue((string)value, out string propname);
             return propname;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            //Ninc svisszafelé konvertálás
-            throw new NotImplementedException();
+            // Nincs visszafelé konvertálás
+            throw new InvalidOperationException("Can not convert backwards");
         }
     }
 }
